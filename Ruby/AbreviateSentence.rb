0eb4b53f17b8385182e35cbf1f3e312687d7e801
Adria1988 <adribuch1988@gmail.com>
2020-04-03 16:00:58 +0200

    IsValidName

--- Ruby/AbreviateSentence.rb
+++ Ruby/AbreviateSentence.rb
@@ -13,5 +13,28 @@ def abbreviate_sentence(sent)
   no_vowels.join(' ')
 end
 
+#
+# OtherSolution
+#
+#  new_words = []
+#  words.each do |word|
+#     if word.length > 4
+#       new_word = abbreviate_word(word)
+#       new_words << new_word
+#     else
+#       new_words << word
+#     end
+#   end
+#
+#   def abbreviate_word(word)
+#   vowels = "aeiou"
+#   new_word = ""
+#   word.each_char do |char|
+#     if !vowels.include?(char)
+#       new_word += char
+#     end
+#   end
+#
+
 puts abbreviate_sentence('follow the yellow brick road') # => "fllw the yllw brck road"
 puts abbreviate_sentence('what a wonderful life')
