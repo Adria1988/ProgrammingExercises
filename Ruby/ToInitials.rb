a83eeddd57c386f46d7ec74e6776e1bf646dfb41
Adria1988 <adribuch1988@gmail.com>
2020-04-03 15:20:19 +0200

    ToInitials

---
+++ Ruby/ToInitials.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# To Initials
+# Write a method to_initials that takes in a person's name string and
+# returns a string representing their initials.
+
+def to_initials(name)
+  array = name.split(' ')
+  initials = ''
+  array.each { |part| initials += part[0] }
+  initials
+ end
+
+puts to_initials('Kelvin Bridges')      # => "KB"
+puts to_initials('Michaela Yamamoto')   # => "MY"
+puts to_initials('Mary La Grange')      # => "MLG"
