321952edeb4e3a8ca9cc021b2c5d971d6569447f
Adria1988 <adribuch1988@gmail.com>
2020-04-05 14:30:39 +0200

    PigLatinWord

---
+++ Ruby/NestedLoops&2DArrays/PigLatinWord.rb
@@ -0,0 +1,21 @@
+#Write a method pig_latin_word that takes 
+#in a word string and translates the word into pig latin.
+
+def pig_latin_word(word)
+  vowels = "aeiou"
+
+  if vowels.include?(word[0])
+     return word + "yay"
+  end
+
+  word.each_char.with_index do |char, i|
+    if vowels.include?(char)
+      return word[i..-1] + word[0...i] + "ay"
+    end
+  end
+end
+
+puts pig_latin_word("apple")   # => "appleyay"
+puts pig_latin_word("eat")     # => "eatyay"
+puts pig_latin_word("banana")  # => "ananabay"
+puts pig_latin_word("trash")   # => "ashtray"
